0b534503bd2579a6578b53a04518b0a61042bc26
dilshod-ship-it <student@localhost>
2020-01-21 11:34:40 +0500

    fixed input category and focus on Summ

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ rootEl.innerHTML = `
 	
 	<button data-action="add">Добавить</button>
 	</form>
+	<ul data-id="purchases-list"></ul>
 	<div id="purchases-total"></div>
 `;
 
@@ -22,17 +23,28 @@ const purchasesCategoryEl = purchasesAddFormEl.querySelector('[data-id=purchases
 
 const purchasesSummEl = purchasesAddFormEl.querySelector('[data-id=purchases-summ]');
 
+const purchasesListEl = rootEl.querySelector('[id=purchases-total]');
+
 let purchasesTotal = 0;
 purchasesAddButtonEl.onclick = evt => {
 	evt.preventDefault();
 
 	// itemEl.innerHTML = `${purchasesCategoryEl.value}`;
 	const value = purchasesSummEl.value;
+	const category = purchasesCategoryEl.value;
 	purchasesTotal +=parseInt(value, 10);
 	purchasesTotalEl.textContent = `Сумма: ${purchasesTotal}`;
 
+	const purchaseEl = document.createElement('li');
+	purchaseEl.innerHTML = `
+		Покупка на сумму ${value}, в категории ${category}
+	`;
+
+	purchasesListEl.insertBefore(purchaseEl, purchasesListEl.firstElementChild);
+
 	purchasesSummEl.value = '';
 	purchasesCategoryEl.value = '';
+	purchasesSummEl.focus(); //браузер настолько умный =, что с другого элемента фокус уберет
 };
 
 
